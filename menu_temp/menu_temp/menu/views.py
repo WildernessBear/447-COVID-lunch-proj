58fe69c192296fca6740d53f26e5fa39ae4ef447
Christina Lukaszczyk <clukas93@gmail.com>
2020-10-31 13:43:35 -0400

    menu/views.py: fixed problem with classes used to hold temporary data

--- menu_temp/menu_temp/menu/views.py
+++ menu_temp/menu_temp/menu/views.py
@@ -2,12 +2,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import SchoolDistrict, School, Menu, Meal
 
-class MenuObj():
-    name = ''
-    meal_ls = []
-    description_ls = []
+# this holds menu information for a single menu
+class MenuObj:
+    def __init__(self, name):
+        self.name = name
+        self.meal_ls = []
     def __str__(self):
         return self.name
+
+# this holds information for a single meal
+class MealObj:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
 
 def menu_view(response, id):
     try:
@@ -16,7 +23,7 @@ def menu_view(response, id):
         # this dictionary will be passed to the template
         context = {
             'school': school.name,
-            'menu_ls': []
+            'menu_ls': [],
         }
 
         text = school.name
@@ -26,14 +33,21 @@ def menu_view(response, id):
             # remove print statements later!
             for menu in school.menu_set.all():
                 print(menu.name)
-                temp_menu = MenuObj()
-                temp_menu.name = menu.name
+                temp_menu = MenuObj(menu.name)
+                #temp_menu.name = menu.name
+
                 for item in menu.meal_set.all():
-                    temp_menu.meal_ls.append(item.name)
-                    temp_menu.description_ls.append(item.description)
+                    temp_meal = MealObj(item.name, item.description)
+                    #temp_meal.name = item.name
+                    #temp_meal.description = item.description
+
+                    temp_menu.meal_ls.append(temp_meal)
+                    print(temp_meal.name)
                     print(' - ', item.name, ': ', item.description)
+
                 context['menu_ls'].append(temp_menu)
-            print(context['menu_ls'][0].meal_ls)
+                for i in temp_menu.meal_ls:
+                    print(i.name)
         else:
             print('menu does not exist')
     except:
