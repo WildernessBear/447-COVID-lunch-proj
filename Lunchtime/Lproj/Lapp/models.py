ea11571600ca9584a1e902acce0b8529eff34667
WildernessBear <littlefoot93@gmail.com>
2020-11-04 17:33:52 -0500

    did model for number of meals per location also password files if I can ever figure out how to reset a password correctly

--- Lunchtime/Lproj/Lapp/models.py
+++ Lunchtime/Lproj/Lapp/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 # custom user class - currently just exists
 class MyUser(AbstractUser):
     pass
+
 
 class SchoolDistrict(models.Model):
     name = models.CharField(max_length=80)
@@ -11,12 +13,14 @@ class SchoolDistrict(models.Model):
     def __str__(self):
         return self.name
 
+
 class School(models.Model):
     district = models.ForeignKey(SchoolDistrict, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=80)
 
     def __str__(self):
         return self.name
+
 
 # menu for maybe the day of the week??
 class Menu(models.Model):
@@ -26,11 +30,24 @@ class Menu(models.Model):
     def __str__(self):
         return self.name
 
+
 # this will hold details of a single meal
 class Meal(models.Model):
     menu = models.ForeignKey(Menu, on_delete=models.CASCADE, null=False)
     name = models.CharField(max_length=80)
     description = models.CharField(max_length=240)
+    preparations = models.CharField(max_length=80)
 
     def __str__(self):
         return self.name
+
+
+# holds info about projected number of meals per day, per location
+class TotalMeals(models.Model):
+    location_name = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
+    date = models.DateField()
+    num_meals = models.IntegerField()
+
+    def __str__(self):
+        return self.location_name
