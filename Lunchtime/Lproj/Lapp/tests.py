f2b342b8ded7904ca998522dcad455c9003039e9
SCrit7 <sarah@greyoak.com>
2020-11-17 21:25:08 -0500

    Testing - register a regular user and log in changed html for login and registration, so their submit buttons now have names, amongst other things test uses selenium, goes to homepage and checks the title. Then, moves to the registration page. Fills out the form with throwaway answers, and hits Register. Checks that 'Thank you for registering' is displayed on screen, moves to login page. There, it inputs the username and password and clicks Login. If moved to the homepage with "Hello [username]" displayed, test finishes successfully Also changed the login so an invalid login doesn't render a new page, but keeps you on the login page with an error message

--- Lunchtime/Lproj/Lapp/tests.py
+++ Lunchtime/Lproj/Lapp/tests.py
@@ -1,17 +1,21 @@
 import os
 import unittest
+import time
 from django.test import TestCase
 from django.test import LiveServerTestCase
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
 # Create your tests here.
 
 HOMEPAGE = 'http://localhost:8000'
 
-#USER_NAME = os.environ['USER_NAME']
-#USER_PASSWORD = os.environ['USER_PASSWORD']
+# USER_NAME = os.environ['USER_NAME']
+# USER_PASSWORD = os.environ['USER_PASSWORD']
 
 
 class FunctionalTests(TestCase):
@@ -23,16 +27,71 @@ class FunctionalTests(TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_homepage_is_good(self):
+    # from the homepage, navigate to the Registration page, create a valid user
+    # then navigate to Login page. Log in using that valid user
+    def test_valid_user_login(self):
         self.browser.get(HOMEPAGE)
-        # self.assertIN('The Lunch Project', self.browser.title)
-        # assert self.browser.title == 'The Lunch Project'
         assert 'The Lunch Project' in self.browser.title
+        # Register a user
+        self.browser.find_element_by_link_text('Register').click()
+
+        username = self.browser.find_element_by_name('username')
+        username.clear()
+        username.send_keys('name')
+
+        password = self.browser.find_element_by_name('password')
+        password.clear()
+        password.send_keys('password')
+
+        email = self.browser.find_element_by_name('email')
+        email.clear()
+        email.send_keys('name@example.com')
+
+        self.browser.find_element_by_name('RegistrationButton').click()
+
+        # verify that the user has been logged in
+        element = WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.ID, 'Registered'))
+        )
+        print(element)
+        if (element == 'Thank you for registering!'):
+            print("Pass: Registered correctly")
+        else:
+            print("Fail: Problem registering")
+
+        # Log that user in
+        self.browser.find_element_by_link_text('Login').click()
+
+        username = self.browser.find_element_by_name('username')
+        username.clear()
+        username.send_keys('name')
+
+        password = self.browser.find_element_by_name('password')
+        password.clear()
+        password.send_keys('password')
+
+        self.browser.find_element_by_name('LoginButton').click()
+
+        # verify that the user has been logged in
+        element = WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.ID, 'LoggedIn'))
+        )
+        print(element)
+        if (element == 'Hello name'):
+            print("Pass: Logged in correctly")
+        else:
+            print("Fail: Problem logging in")
+
+        # self.browser.find_element_by_partial_link_text('Home')
+        time.sleep(2)
 
 
-    def test_login_is_good(self):
-        self.browser.get(HOMEPAGE)
-        # self.assertIN('The Lunch Project', self.browser.title)
+        # finds elements in the nav bar, but just selects the first one: ie Admin
+        # self.browser.find_element_by_class_name('navbar-link').click()
+
+
+    # def test_login_is_good(self):
+        # self.browser.get(HOMEPAGE)
 
 # if __name__ == '__main__':
 #   unittest.main()
--- Lunchtime/Lproj/Lapp/views.py
+++ Lunchtime/Lproj/Lapp/views.py
@@ -47,19 +47,22 @@ def register(request):
 
 def user_login(request):
     if request.method == 'POST':
+        valid_user = False
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(username=username, password=password)
         if user:
             if user.is_active:
                 login(request, user)
+                valid_user = True
                 return HttpResponseRedirect(reverse('index'))
             else:
                 return HttpResponse("Your account was inactive.")
         else:
             print("Someone tried to login and failed.")
             print("They used username: {} and password: {}".format(username, password))
-            return HttpResponse("Invalid login details given")
+            # return HttpResponse("Invalid login details given")
+            return render(request, 'Lapp/login.html', {'valid_user': not valid_user})
     else:
         return render(request, 'Lapp/login.html', {})
 
