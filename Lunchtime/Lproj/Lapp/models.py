20f21928b68eb3a5ac9fec63eb042a953b17d282
Christina Lukaszczyk <clukas93@gmail.com>
2020-12-01 02:11:57 -0500

    commiting before pulling from master

--- Lunchtime/Lproj/Lapp/models.py
+++ Lunchtime/Lproj/Lapp/models.py
@@ -48,7 +48,7 @@ class Meal(models.Model):
     menu = models.ForeignKey(Menu, on_delete=models.CASCADE, null=False)
     name = models.CharField(max_length=80)
     description = models.CharField(max_length=240)
-    prep = models.CharField(max_length=240)
+    prep = models.CharField(max_length=240, null=True)
 
     def __str__(self):
         return self.name
