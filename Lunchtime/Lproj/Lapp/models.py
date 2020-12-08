252124a82e03dd3feb3c37e7168c2e1e9793f99f
Christina Lukaszczyk <clukas93@gmail.com>
2020-12-08 03:42:37 -0500

    created an example database with realistic data

--- Lunchtime/Lproj/Lapp/models.py
+++ Lunchtime/Lproj/Lapp/models.py
@@ -56,7 +56,7 @@ class Meal(models.Model):
 
 class Ingredient(models.Model):
     meal = models.ForeignKey(Meal, on_delete=models.CASCADE, null=False)
-    name = models.CharField(max_length=240)
+    name = models.CharField(max_length=240, null=True)
 
     def __str__(self):
         return self.name
